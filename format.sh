4d59c42b5fd4584dd54a302971d58a22bc54e780
salvatore rivieccio <salvatorerivieccio@duck.com>
2025-12-18 11:23:59 +0100

    Add CMake build configuration and update .gitignore

--- format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for f in $(rg . -l -t cpp); do
-	clang-format $f | rewrite $f
-done
--- valgrind.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# yes | valgrind --suppressions=suppressions.txt --gen-suppressions=yes --leak-check=full --show-leak-kinds=all "$@"
-yes | valgrind --leak-check=full --show-leak-kinds=all "$@"
